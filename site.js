03dd0d0565e81170136f56cfab5539853a3e6a6d
Phill Conrad <pconrad@cs.ucsb.edu>
2016-06-12 15:58:10 -0700

    refactor javascript for simpler html

--- site.js
+++ site.js
@@ -4,11 +4,10 @@ layout: null
 
 $(document).ready(function(){
     console.log("site.js: document is ready");
-    $('.hfj').each(function() {
+    $('[data-hfj]').each(function() {
 	var chapter_num = $(this).data('hfj');
 	var href = "{{ site.hfj_chapter_url_prefix }}" + chapter_num;
-	console.log("site.js: chapter_num=" + chapter_num + " href=" + href);
 	$(this).html('<a href="' + href + '">HFJ Chapter ' + $(this).data('hfj') + '</a>');
     });
-
+    console.log("site.js: done");
 });
