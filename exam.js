9753f66cf909c54b8585295fb21ad702ccf57583
Phill Conrad <pconrad@cs.ucsb.edu>
2016-07-04 14:55:50 -0700

    exam formatting

--- exam.js
+++ exam.js
@@ -12,17 +12,21 @@ $(document).ready(function(){
     $('.page-break-before').each(function() {
 	var prev = $(this).prev();
 	var $div = $("<div>", {class: "pagebreak"});
-	prev.css('background-color','pink');
 	prev.append($div);
 	$('.exam-page-header-template').first().clone().appendTo($div);
+	$('.exam-name-header-template').first().clone().appendTo($div);
 	prev.css('margin-bottom','0');
     });
 
 
     $('td.page-num').each(function(i) {
-	$(this).html(i+1); // re-calculate page numbers
+	var pageNum = i+1;
+	$(this).html(pageNum); // re-calculate page numbers
+	$(this).data("pageNum",pageNum);
+	if (pageNum % 2==0) {
+	    $(this).parents(".pagebreak").find(".exam-name-header-table").css("display","none");
+	}
     });
-
 
     console.log("exam.js: done");
 });
