5c2ca7073d3df5103d9760a1628b2e68b0e54e29
hiranya <hiranya911@gmail.com>
2016-07-13 14:01:07 -0700

    Fixing the page number generation on homeworks

--- hwk.js
+++ hwk.js
@@ -8,12 +8,12 @@ layout: null
 $(document).ready(function(){
     console.log("hwk.js: document is ready");
 
-    $('td.page-num').each(function(i) {
-	$(this).html(i+1); // re-calculate page numbers
-    });
-
     $('.pagebreak').each(function() {
 	$('.hwk-page-header-template').first().clone().appendTo($(this));
+    });
+
+    $('td.page-num').each(function(i) {
+	$(this).html(i+1); // re-calculate page numbers
     });
     
     console.log("hwk.js: done");
